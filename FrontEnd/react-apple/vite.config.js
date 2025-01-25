2ae1a677e94558bccf82f1e1e7d74b48b0d953c9
Hagosw <noelh0618@gmail.com>
2025-01-25 13:12:36 -0500

    Latest version

--- FrontEnd/react-apple/vite.config.js
+++ FrontEnd/react-apple/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  // server: { port: 1057 },
+  server: { port: 1057 },
   build: {  
     sourcemap: true, 
-    base: "/Full-Stack-Apple-Website",
   }
-  
 });
